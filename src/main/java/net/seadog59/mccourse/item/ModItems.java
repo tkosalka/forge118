55373ca12a8d2e161a10dca8326ec3aea82acac4
kosalka <tkosalka@yahoo.com>
2023-05-30 23:10:53 -0400

    adding cobbalt ignot and nugget

--- src/main/java/net/seadog59/mccourse/item/ModItems.java
+++ src/main/java/net/seadog59/mccourse/item/ModItems.java
@@ -14,6 +14,9 @@ public class ModItems {
    public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
