00bedeb00dc8c78aeeef79f9345fe43d5edea553
kosalka <tkosalka@yahoo.com>
2023-05-31 21:05:01 -0400

    adding cobalt block wipt

--- src/main/java/net/seadog59/mccourse/MCCourseMod.java
+++ src/main/java/net/seadog59/mccourse/MCCourseMod.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.seadog59.mccourse.block.ModBlocks;
 import net.seadog59.mccourse.item.ModItems;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -31,6 +32,7 @@ public class MCCourseMod {
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
         eventBus.addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
